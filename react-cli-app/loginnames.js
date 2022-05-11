f89eca08677f166bdde797fceb4f61f24edda084
malikvedanshu <malikvedanshu757@outlook.com>
2022-05-11 23:11:37 +0000

    pendingCheckbox

--- react-cli-app/loginnames.js
+++ react-cli-app/loginnames.js
@@ -2,18 +2,33 @@ import axios from "axios";
 
 async function loginnamesGithub(){
     try{
-        let res = await axios.get('https://api.github.com/users');
-        let names = res.data.map(user => user.login)
-        // names = names.sort((a,b) => b.login-a.login);
-        names = names.sort();
-        console.log(names);
+        const res = await axios.get('https://api.github.com/users', {
+            auth: {
+                username: 'MalikVedanshu',
+                password: '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'
+            }
+        });
+        // console.log(res.data);
+        for(let i = 0; i < res.data.length; i++){
+            let user = await axios.get(`https://api.github.com/users/${res.data[i].login}`, {
+            auth: {
+                username: 'MalikVedanshu',
+                password: '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'
+                }
+            });
+            res.data[i].followers = user.data.followers;
+            console.log(res.data);
+        }
+        console.log(res.data);
     }
     
     catch (err){
-        console.log('something went wrong')
+        console.log('something went wrong');
     }
 
 }
+
+
 loginnamesGithub();
 
 export default loginnamesGithub;
