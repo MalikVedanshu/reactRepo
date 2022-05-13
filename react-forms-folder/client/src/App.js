4176b9aa36d4e08f181be94502580ce60d4e501a
malikvedanshu <malikvedanshu757@outlook.com>
2022-05-13 01:09:22 +0000

    3 empty repos for project

--- react-forms-folder/client/src/App.js
+++ react-forms-folder/client/src/App.js
@@ -12,42 +12,56 @@ class App extends Component {
                 techstack: '',
                 birthday: '',
                 profileurl: '',
-                bio: ''
+                bio: '',
+                interests: []
             }
         }
     }
-
     onChange = (event) => {
-        // console.log(event.target.name,event.target.value);
         this.setState({
-            userdata : {
-                this.state.userdata,
+            userdata: {
+                ...this.state.userdata,
                 [event.target.name]: event.target.value
             }
-        })
+        });
     }
-
-    
-    
-    
+    checkBoxHandler = (event) => {
+        let interests = this.state.userdata.interests;
+        event.target.checked ? interests.push(event.target.value) : interests = interests.filter(skill => skill !== event.target.value);
+        this.setState({
+            userdata: {
+                ...this.state.userdata,
+                interests: interests
+            }
+        });
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.userdata);
+    }
     render() {
+        const { fname, lname, email, profileurl, bio } = this.state.userdata;
         return (
             <div className="formdata">
                 <center>
                     <h1> Customer Form </h1>
-                    <form>
+                    <form onSubmit={this.onSubmit}>
                         <label htmlFor="fname">First name:</label>
-                        <input type="text" name="fname" onChange={this.onChange} required/> <br /><br />
+                        <input type="text" name="fname"
+                            onChange={this.onChange}
+                            required
+                            value={fname}
+                        /> <br /><br />
                         <label htmlFor="lname" >Last name:</label>
-                        <input type="text" name="lname" onChange={this.onChange} required/><br /><br />
+                        <input type="text" name="lname" onChange={this.onChange} required value={lname} /><br /><br />
 
-                        <label htmlFor="Email">Enter your email:</label>
-                        <input type="email" name="email" onChange={this.onChange} required /><br /><br />
+                        <label htmlFor="email">Enter your email:</label>
+                        <input type="email" name="email" onChange={this.onChange} required value={email} /><br /><br />
 
                         <p>Choose your gender :</p>
-                        <input type="radio" name="gender" onChange={this.onChange} /><label htmlFor="html"> Male </label>
-                        <input type="radio" name="gender" onChange={this.onChange} /><label htmlFor="html"> Female </label>
-                        <input type="radio" name="gender" onChange={this.onChange} /><label htmlFor="html"> Other </label><br /><br />
+                        <input type="radio" name="gender" value="Male" onChange={this.onChange} /><label htmlFor="html"> Male </label>
+                        <input type="radio" name="gender" value="Female" onChange={this.onChange} /><label htmlFor="html"> Female </label>
+                        <input type="radio" name="gender" value="Other" onChange={this.onChange} /><label htmlFor="html"> Other </label><br /><br />
 
                         <label htmlFor="techstack">Choose your tech stack :</label>
                         <select name="techstack" onChange={this.onChange}>
@@ -59,14 +73,25 @@ class App extends Component {
                             <option value="python">Python</option>
                         </select><br /><br />
                         <br />
+                        <label htmlFor="interests"> UI Development</label>
+                        <input type="checkbox" value="UI Development" name="interests" onChange={this.checkBoxHandler} />
 
+                        <label htmlFor="interests"> Backend Development</label>
+                        <input type="checkbox" value="Backend Development" name="interests" onChange={this.checkBoxHandler} />
+
+                        <label htmlFor="interests"> Cloud Architect </label>
+                        <input type="checkbox" value="Cloud Architect" name="interests" onChange={this.checkBoxHandler} />
+
+                        <label htmlFor="interests"> Digital Marketing </label>
+                        <input type="checkbox" value="Digital Marketing" name="interests" onChange={this.checkBoxHandler} />
+                        <br /><br />
                         <label htmlFor="dob">Date of Birth:  </label>
                         <input type="date" name="birthday" onChange={this.onChange}></input><br /><br />
                         <label htmlFor="profile">Enter your twitter/linked in profile URL : </label>
-                        <input type="url" name="profileurl" onChange={this.onChange} /> <br /><br />
+                        <input type="url" name="profileurl" onChange={this.onChange} value={profileurl} /> <br /><br />
                         <label htmlFor="bio">Enter your Bio :</label> <br />
 
-                        <textarea name="bio" onChange={this.onChange} rows="4" cols="50">
+                        <textarea name="bio" rows="4" cols="50" onChange={this.onChange} value={bio}>
 
                         </textarea>
                         <br /><br />
