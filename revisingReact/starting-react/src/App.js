c255be4683dba2f1cf2778b93d6be88651ebb8ef
malikvedanshu <malikvedanshu757@outlook.com>
2022-08-04 16:09:08 +0000

    jsx babble

--- revisingReact/starting-react/src/App.js
+++ revisingReact/starting-react/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Fragment} from 'react';
+let adj = 'funny';
+let myAdj = 'smarter';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <h1> Hello {adj} World </h1>
+      <h2> I'm becoming {myAdj} </h2>
+    </Fragment>
+  )
 }
-
 export default App;
