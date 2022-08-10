86bf07c096061f83b42fd53cacd4a6e8ec8b503e
malikvedanshu <malikvedanshu757@outlook.com>
2022-08-10 19:48:25 +0000

    classes 2

---
+++ revisingReact/understanding-classes/src/1.js
@@ -0,0 +1,18 @@
+const { createContext } = require("react");
+
+class Members{
+    constructor(name,phone){
+        this.name = name;
+        this.phone = phone;
+        this.hobby = [];
+    }
+    addHobby(hobby){
+        this.hobby.push(hobby);
+    }
+}
+let ved = new Members('vedanshu','954654426');
+console.log(ved);
+ved.addHobby('swimming');
+ved.addHobby('boxing');
+ved.addHobby('jua');
+console.log(ved);
